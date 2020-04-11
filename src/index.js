d5d2fc40451e0c6669452b2dcb702403c63aebd6
Casta <palolahrn0104@gmail.com>
2020-04-11 12:53:38 -0500

    Se actualizó el proyecto con bootstrap

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './pages/Home';
-import 'bulma/css/bulma.css';
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 
 ReactDOM.render(
