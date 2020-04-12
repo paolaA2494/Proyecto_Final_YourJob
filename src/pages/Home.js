d8bb6b603cfef1daddf550e1e1f54f00a0821590
Mariana Loaiza <mariloaiza20@gmail.com>
2020-04-12 02:50:26 -0500

    Se agrega las tarjetas

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,14 +3,79 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import Searchbar from '../components/Searchbar';
 import Login from '../components/Login';
+import Card from '../components/Card';
 
 
-function Home() {
-  return (
-    <>
-    <Searchbar />
-    </>
-  );
+  class Home extends React.Component {
+        constructor(props){
+          super(props);
+
+        this.state = {
+              data:[]
+          }
+      }
+
+
+      componentDidMount(){
+        this.timeoutId = setTimeout(() => {
+              this.setState({
+                  data:[
+                      {
+                          id: '2de30c42-9deb-40fc-a41f-05e62b5939a7',
+                          Name: 'Mariana Loaiza',
+                          calificacion: '4.7',
+                          img:
+                          'https://www.gravatar.com/avatar?d=identicon',
+                          descripcion:'Lorem ipsum dolor sit amet.',
+                        
+                        },
+                        {
+                          id: 'd00d3614-101a-44ca-b6c2-0be075aeed3d',
+                          Name: 'Leina Pulgarin',
+                          calificacion: '4.9',
+                          img:
+                          'https://www.gravatar.com/avatar?d=identicon',
+                          descripcion:'Lorem ipsum dolor sit amet.',                  
+                        },
+                        {
+                          id: '63c03386-33a2-4512-9ac1-354ad7bec5e9',
+                          Name: 'Paola Hernadez',
+                          calificacion: '4.9',
+                          img:
+                          'https://www.gravatar.com/avatar?d=identicon',
+                          descripcion:'Lorem ipsum dolor sit amet.',
+                      },
+                  ]
+              })
+          }, 3000 ) }
+
+          componentDidUpdate(prevProps, prevState){
+            console.log({
+              prevProps: prevProps, prevState: prevState
+            })
+
+            console.log({
+                props: this.props,
+                state: this.state
+            })
+          }
+
+          componentWillUnmount(){
+              clearTimeout(this.timeoutId)
+          }
+
+          
+        render(){
+        return (
+          
+          <React.Fragment>
+            <Searchbar />
+
+            <Card info={this.state.data} />
+          </React.Fragment>
+          
+    );
+  }
 }
 
 export default Home;
