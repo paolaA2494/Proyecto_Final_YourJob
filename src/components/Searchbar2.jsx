dc6d141c1b21598a60547a5b3339580bc28aef03
Leina Pulgarin <leinatamayo@gmail.com>
2020-04-15 23:23:09 -0500

    Se modificaron detalles en las card y en la pag principal

--- src/components/Searchbar2.jsx
+++ src/components/Searchbar2.jsx
@@ -3,13 +3,11 @@ import "./App.css";
 import { Link } from "react-router-dom";
 
 class Searchbar extends Component {
-  
-  handleSelectChange= (evet) =>{
+  handleSelectChange = (evet) => {
+    const { handlChange } = this.props;
+    handlChange(evet.target.value);
+  };
 
-    const {handlChange} = this.props
-    handlChange(evet.target.value) 
-    
-  }
   render() {
     return (
       <main className="inner2">
@@ -18,7 +16,13 @@ class Searchbar extends Component {
             <strong>Busca aquí tu servicio</strong>
           </h2>
           <div className="row d-flex justify-content-center">
-            <select className="custom-select col-8" id="country" required="" name="select-service" onChange={this.handleSelectChange}>
+            <select
+              className="custom-select col-8"
+              id="country"
+              required=""
+              name="select-service"
+              onChange={this.handleSelectChange}
+            >
               <option value="">Seleccione...</option>
               <option value="plomeria">Plomeria</option>
               <option value="estetica">Estética</option>
@@ -26,10 +30,9 @@ class Searchbar extends Component {
               <option value="gaseoducto">Mantenimiento de gaseoductos</option>
             </select>
             <div className="mx-auto col-3">
-                <button className="btn btn-dark btn-block" type="submit">
-                  Buscar
-                </button>
-             
+              <button className="btn btn-dark btn-block" type="submit">
+                Buscar
+              </button>
             </div>
           </div>
         </div>
