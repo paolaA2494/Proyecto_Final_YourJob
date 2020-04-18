f506e3aa4b9dbaf483da720e21222ba299062456
Leina Pulgarin <leinatamayo@gmail.com>
2020-04-18 01:45:35 -0500

    actualizando el la ruta local, por el EndPoint en la nube

--- src/pages/ShowCard.js
+++ src/pages/ShowCard.js
@@ -9,55 +9,42 @@ class ShowCard extends React.Component {
 
     this.state = {
       data: [],
-      filter: '',
+      filter: "",
     };
   }
 
   componentDidMount() {
-    this.getData()
+    this.getData();
   }
-
 
   getData = async () => {
     try {
-      const { data } = await axios.get('http://localhost:8080/workers')
+      const { data } = await axios.get("https://your-job.now.sh/worker");
       this.setState({
-        data
-      })
-    } catch (error) {
-
-    }
-
-  }
-
-
+        data,
+      });
+    } catch (error) {}
+  };
 
   selectedSearchOption = (service) => {
     console.log(service);
     this.setState({ filter: service });
   };
 
-
-
   render() {
-
-    console.log(this.state)
-    let fullData = this.state.data
-
+    console.log(this.state);
+    let fullData = this.state.data;
 
     if (this.state.filter !== "") {
       fullData = this.state.data.filter(
         (item) => item.serviceType === this.state.filter
-      )
+      );
     }
-
 
     return (
       <React.Fragment>
         <Searchbar2 handlChange={this.selectedSearchOption} />
-        <Card
-          info={fullData}
-        />
+        <Card info={fullData} />
       </React.Fragment>
     );
   }
